2a5f98102719353bcbab63869dc39337867125a0
dvdkkk <124906710+dvdkkk@users.noreply.github.com>
2026-08-25 14:14:11 +0900

    Remove redundant license comment in bundle.js

--- bundle.js
+++ bundle.js
@@ -143,7 +143,7 @@ Error generating stack: `+a.message+`
  * See the LICENSE file in the root directory of this source tree.
  */const Jm=[["path",{d:"M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384",key:"9njp5v"}]],Fm=He("phone",Jm);/**
  * @license lucide-react v0.546.0 - ISC
- *
+ * 
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
  */const Wm=[["path",{d:"M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z",key:"1ffxy3"}],["path",{d:"m21.854 2.147-10.94 10.939",key:"12cjpa"}]],ko=He("send",Wm);/**
